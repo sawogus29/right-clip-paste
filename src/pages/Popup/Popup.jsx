019d55607b6ab615a3a57b783e2a09b4fe9cfa6d
Jaehyeon Park <sawogus29@gmail.com>
2022-12-25 17:07:11 +0900

    feat(Popup): implement Popup

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,74 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
+import React, { useEffect, useState } from 'react';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { FormGroup } from '@mui/material';
 
-const Popup = () => {
+function Popup() {
+  const [isRightClipOn, setIsRightClipOn] = useState(true);
+  const [isHoverEffectOn, setIsHoverEffectOn] = useState(true);
+  const [isPasteToTextInputOn, setIsPasteToTextInputOn] = useState(true);
+
+  // initialization
+  useEffect(() => {
+    chrome.storage.local
+      .get({
+        isRightClipOn: true,
+        isHoverEffectOn: true,
+        isPasteToTextInputOn: true,
+      })
+      .then((result) => {
+        const { isRightClipOn, isHoverEffectOn, isPasteToTextInputOn } = result;
+        setIsRightClipOn(isRightClipOn);
+        setIsHoverEffectOn(isHoverEffectOn);
+        setIsPasteToTextInputOn(isPasteToTextInputOn);
+      });
+  }, []);
+
+  // switch handler
+  useEffect(() => {
+    chrome.storage.local.set({ isRightClipOn, isHoverEffectOn: isRightClipOn });
+  }, [isRightClipOn]);
+  useEffect(() => {
+    chrome.storage.local.set({ isHoverEffectOn });
+  }, [isHoverEffectOn]);
+  useEffect(() => {
+    chrome.storage.local.set({ isPasteToTextInputOn });
+  }, [isPasteToTextInputOn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <FormGroup className="form-group">
+        <FormControlLabel
+          label="right click to clip"
+          control={
+            <Switch
+              checked={isRightClipOn}
+              onChange={(e) => setIsRightClipOn(e.target.checked)}
+            />
+          }
+        />
+        <FormControlLabel
+          label="hover effect"
+          control={
+            <Switch
+              checked={isHoverEffectOn}
+              onChange={(e) => setIsHoverEffectOn(e.target.checked)}
+              disabled={!isRightClipOn}
+            />
+          }
+        />
+        <FormControlLabel
+          label="paste to text input"
+          control={
+            <Switch
+              checked={isPasteToTextInputOn}
+              onChange={(e) => setIsPasteToTextInputOn(e.target.checked)}
+            />
+          }
+        />
+      </FormGroup>
     </div>
   );
-};
+}
 
 export default Popup;
